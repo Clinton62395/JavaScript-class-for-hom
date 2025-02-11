d3397fc92d556df4af6a29dd84f49e68a7bcd063
BILLY <billydoumbouya5210@gmail.com>
2025-02-12 00:37:22 +0100

    add my new files

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// alert("welcome to my javascript page !");
+
+// let Nom = "Billy";
+// let Age = 25;
+// console. log("nom");
+// console.log("age");
+
+function changerTexte() {
+    document.getElementById("demo").innerHTML = "Welcome to my javascript !"
+}
+document.getElementById("mybouton").addEventListener("click",
+    function() {
+        alert("you have clicked !");
+    });
+
